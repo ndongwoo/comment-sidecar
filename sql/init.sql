6cf9c241367d3c64cc8fda164ce6cd95791c6ca4
OpenAI <noreply@openai.com>
2026-09-16 23:49:41 +0000

    fix: support standard PDO DSN and utf8mb4

--- sql/init.sql
+++ sql/init.sql
@@ -13,7 +13,7 @@ CREATE TABLE comments (
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   -- don't change the name 'replyTo_refers_to_existing_id' without adapting the referring php code.
   CONSTRAINT replyTo_refers_to_existing_id FOREIGN KEY comments(reply_to) REFERENCES comments(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE INDEX read_index ON comments (`site`, `path`, `creation_date`);
 
@@ -23,4 +23,4 @@ DROP TABLE IF EXISTS ip_addresses;
 CREATE TABLE ip_addresses (
     `ip` varchar(45) NOT NULL,
     `creation_date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- src/common.php
+++ src/common.php
@@ -1,15 +1,15 @@
 <?php
 ob_start("ob_gzhandler");
-include_once "config.php";
+include_once __DIR__ . "/config.php";
 
 function connect() {
-    $handler = new PDO("mysql:host=".DB_HOST.":".DB_PORT.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PW);
+    $handler = new PDO("mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=utf8mb4", DB_USER, DB_PW);
     $handler->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     return $handler;
 }
 
 function readTranslations(): array  {
-    $translationFile = 'translations/'. LANGUAGE .'.php';
+    $translationFile = __DIR__ . '/translations/'. LANGUAGE .'.php';
     if (!file_exists($translationFile)) {
         http_response_code(500);
         echo "Can't find translation file $translationFile";
